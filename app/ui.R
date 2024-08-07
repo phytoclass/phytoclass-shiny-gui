5fdb8e2367fe6a7d46da3adb88218afd69efcda6
Tylar <murray.tylar@gmail.com>
2024-08-07 10:16:12 -0400

    + more images along bottom

--- app/ui.R
+++ app/ui.R
@@ -17,93 +17,103 @@ ui <- fluidPage(
 
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
-  # Sidebar panel for inputs ----
-  sidebarPanel(
-    img(src='vertical_collage.jpg', width="100%%"),
-    width = 2
-  ),
-  # Main panel for displaying outputs ----
-  mainPanel(
-    # Output: Tabset  ----
-    tabsetPanel(type = "tabs",
-      tabPanel("Upload Data Files",
-        tabsetPanel(type = "tabs",
-          tabPanel("Pigment Samples",
-            markdown(paste0(
-              "# Pigment Sample Matrix \n",
-              "Select a pigment concentrations file to supply the ",
-              "`Sample Matrix` (aka `S matrix`) of pigment samples. \n",
-              "[See here for details]",
-              "(https://github.com/USF-IMARS/chemtax-shiny-gui/blob/main/rmd/pigment_matrix.md)"
-            )),
-            fileInput("pigments_file", "Pigments .csv file.",
-              multiple = FALSE,
-              accept = c("text/csv",
-                "text/comma-separated-values,text/plain",
-                ".csv"
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      img(src='img/vertical_collage.jpg', width="100%%"),
+      width = 2
+    ),
+    # Main panel for displaying outputs ----
+    mainPanel(
+      # Output: Tabset  ----
+      tabsetPanel(type = "tabs",
+        tabPanel("Upload Data Files",
+          tabsetPanel(type = "tabs",
+            tabPanel("Pigment Samples",
+              markdown(paste0(
+                "# Pigment Sample Matrix \n",
+                "Select a pigment concentrations file to supply the ",
+                "`Sample Matrix` (aka `S matrix`) of pigment samples. \n",
+                "[See here for details]",
+                "(https://github.com/USF-IMARS/chemtax-shiny-gui/blob/main/rmd/pigment_matrix.md)"
+              )),
+              fileInput("pigments_file", "Pigments .csv file.",
+                multiple = FALSE,
+                accept = c("text/csv",
+                  "text/comma-separated-values,text/plain",
+                  ".csv"
+                )
               )
-            )
-            # TODO: toggle pigments on/off
-          ),
-          tabPanel("Taxa List",
-            markdown(paste(
-              "# Taxa list",
-              "List of taxa expected in the sample.",
-              sep = "\n"
+              # TODO: toggle pigments on/off
             ),
-            # TODO: OPTIONAL section
-            # csv upload to customize ratios and|or add rows to userMinMax
-            #       allow download the default table, allow edits
-            # `Ratio Matrix` (aka `F matrix`) is the ratio of pigments
-            #       relative to chlorophyll a.
-            # TODO: select preset dropdown (region)
-            selectInput("taxaPreset", "Taxa Preset", list("all", "antarctic")),
-            # TODO: or custom preset upload
-            fileInput("taxalist_file", "List of taxa .csv file.",
-              multiple = FALSE,
-              accept = c("text/csv",
-                         "text/comma-separated-values,text/plain",
-                         ".csv"
+            tabPanel("Taxa List",
+              markdown(paste(
+                "# Taxa list",
+                "List of taxa expected in the sample.",
+                sep = "\n"
+              )),
+              # TODO: OPTIONAL section
+              # csv upload to customize ratios and|or add rows to userMinMax
+              #       allow download the default table, allow edits
+              # `Ratio Matrix` (aka `F matrix`) is the ratio of pigments
+              #       relative to chlorophyll a.
+              # TODO: select preset dropdown (region)
+              selectInput("taxaPreset", "Taxa Preset", list("all", "antarctic")),
+              # TODO: or custom preset upload
+              fileInput("taxalist_file", "List of taxa .csv file.",
+                multiple = FALSE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv"
+                )
+              )
+              # TODO: ability to customize - uncheck groups in the preset
+              #       example removal:
+              #       Sm2 <- Sm[, -4]
+            )
+          )
+        ),
+          tabPanel("Run Clustering",
+            quartoReportUI("cluster",
+              defaultSetupCode = paste(
+                "inputFile <- 'pigments.rds'",
+                "outputFile <- 'clusters.rds'",
+                sep="\n"
               )
             )
-            # TODO: ability to customize - uncheck groups in the preset
-            #       example removal:
-            #       Sm2 <- Sm[, -4]
+            # TODO: save clusters .csv
+          ),
+          tabPanel("Inspect a Cluster",
+            quartoReportUI("inspectCluster",
+              defaultSetupCode = "selectedCluster <- 1"
+            )
+          ),
+          tabPanel("Run Annealing on a Cluster",
+            # TODO: seed input & explanation
+            quartoReportUI("anneal",
+              # TODO: fill these to match .qmd
+              defaultSetupCode = paste(
+                "inputFile <- 'clusters.rds'",
+                "taxaFile <- 'taxa.rds'",
+                "outputFile <- 'annealing.rds'",
+                "seed <- 0",
+                "selected_cluster <- 1",
+                "niter <- 10",
+                sep="\n"
+              )
+            )
           )
-        )
-      ),
-      tabPanel("Run Clustering",
-        quartoReportUI("cluster",
-          defaultSetupCode = paste(
-            "inputFile <- 'pigments.rds'",
-            "outputFile <- 'clusters.rds'",
-            sep="\n"
-          )
-        )
-        # TODO: save clusters .csv
-      ),
-      tabPanel("Inspect a Cluster",
-        quartoReportUI("inspectCluster",
-          defaultSetupCode = "selectedCluster <- 1"
-        )
-      ),
-      tabPanel("Run Annealing on a Cluster",
-        # TODO: seed input & explanation
-        quartoReportUI("anneal",
-          # TODO: fill these to match .qmd
-          defaultSetupCode = paste(
-            "inputFile <- 'clusters.rds'",
-            "taxaFile <- 'taxa.rds'",
-            "outputFile <- 'annealing.rds'",
-            "seed <- 0",
-            "selected_cluster <- 1",
-            "niter <- 10",
-            sep="\n"
-          )
-        )
+        ),
+      width = 10
       )
     ),
-    width = 10
+  fluidRow(
+    img(src='img/footer_1.jpg', width="12%"),
+    img(src='img/footer_2.jpg', width="12%"),
+    img(src='img/footer_3.jpg', width="12%"),
+    img(src='img/footer_4.jpg', width="12%"),
+    img(src='img/footer_5.jpg', width="12%"),
+    img(src='img/footer_6.jpg', width="12%"),
+    img(src='img/footer_7.jpg', width="12%"),
+    img(src='img/footer_8.jpg', width="12%")
   )
-)
 )
